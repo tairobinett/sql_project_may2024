f7fb2882f08be9c83443b906edbf0f878046efe7
Tai Robinett <tairobinett@gmail.com>
2024-06-06 18:03:40 -0400

    Modified frontend, added button that prints hello world to console.

--- backend/execute-query.py
+++ backend/execute-query.py
@@ -22,6 +22,7 @@ cursor = connection.cursor()
 sql = '''CREATE TABLE IF NOT EXISTS dice_rolls(
     emp_id INT PRIMARY KEY,
     emp_name VARCHAR(255),
+    die_type INT,
     d2 INT,
     d3 INT,
     d4 INT, 
@@ -35,17 +36,27 @@ sql = '''CREATE TABLE IF NOT EXISTS dice_rolls(
 
 cursor.execute(sql)
 
-print("Current active directory:", os.getcwd())
-print("Contents of backend directory:", os.listdir("/app"))
-print("Contents of dice-stats directory:", os.listdir("/app/dice-stats"))
+connection.commit()
 
+#print("Current active directory:", os.getcwd())
+#print("Contents of backend directory:", os.listdir("/app"))
+#print("Contents of dice-stats directory:", os.listdir("/app/dice-stats"))
 
-sql = '''COPY dice_rolls 
-    FROM '/app/dice-stats/data.csv' 
-    DELIMITER ',' CSV HEADER;
-'''
+csv_file_path = '/app/dice-stats/data.csv'
+if os.path.exists(csv_file_path):
+    with open(csv_file_path, 'r') as f:
+        sql_copy = '''COPY dice_rolls(die_type, d2, d3, d4, d6, d8, d10, d12, d20, d100) FROM STDIN WITH CSV HEADER DELIMITER ',';'''
+        cursor.copy_expert(sql_copy, f)
+    print("Data copied from CSV file to 'dice_rolls' table.")
+else:
+    print(f"CSV file '{csv_file_path}' does not exist.")
 
-cursor.execute(sql)
+#sql = '''COPY dice_rolls 
+#    FROM '/app/dice-stats/data.csv' 
+#    DELIMITER ',' CSV HEADER;
+#'''
+
+#cursor.execute(sql)
 
 connection.commit()
 cursor.close()
--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    console.log("Hello world!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={handleClick}>Click me!</button>
         <a
           className="App-link"
           href="https://reactjs.org"
